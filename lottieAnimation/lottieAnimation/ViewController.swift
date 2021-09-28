910dcebc422353506a68e1162f48e64f2fcac379
Ignatio <ignatio.tara@multipolar.com>
2021-09-28 13:59:50 +0800

    using storyboard

--- lottieAnimation/lottieAnimation/ViewController.swift
+++ lottieAnimation/lottieAnimation/ViewController.swift
@@ -12,24 +12,38 @@ import Lottie
 
 class ViewController: UIViewController {
 
-    let animationView = AnimationView()
+    @IBOutlet weak var animationView: AnimationView!
+    
+    //    let animationView = AnimationView()
+//    let label = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupAnimation()
+//        setupLabel()
     }
     
     private func setupAnimation() {
-        animationView.animation = Animation.named("plant-growing-animation")
-        animationView.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
-        animationView.center = view.center
+        animationView.animation = Animation.named("pizza")
+//        animationView.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
+//        animationView.center = view.center
         animationView.backgroundColor = .clear
-        animationView.contentMode = .scaleAspectFit
+//        animationView.contentMode = .scaleAspectFit
         animationView.loopMode = .loop
         animationView.play()
         view.addSubview(animationView)
     }
+
+//    private func setupLabel() {
+//        label.text = "Pleaase wait, your food is delivered to you"
+//        label.frame = CGRect(x: 0, y: 100, width: 300, height: 50)
+//        label.textAlignment = .center
+//        label.contentMode = .scaleAspectFit
+//
+//        view.addSubview(label)
+//
+//    }
 
 
 }
