ee470df372d4ce2ee89e30e36d2f1b883dec39e3
Ignatio <ignatio.tara@multipolar.com>
2021-09-28 13:27:53 +0800

    setup and add lottie animation

--- lottieAnimation/lottieAnimation/ViewController.swift
+++ lottieAnimation/lottieAnimation/ViewController.swift
@@ -6,12 +6,27 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
 
+    let animationView = AnimationView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupAnimation()
+    }
+    
+    private func setupAnimation() {
+        animationView.animation = Animation.named("plant-growing-animation")
+        animationView.frame = CGRect(x: 0, y: 0, width: 400, height: 400)
+        animationView.center = view.center
+        animationView.backgroundColor = .clear
+        animationView.contentMode = .scaleAspectFit
+        animationView.loopMode = .loop
+        animationView.play()
+        view.addSubview(animationView)
     }
 
 
